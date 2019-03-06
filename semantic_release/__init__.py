4c0842e98e41c2fde0105ace03d8f77e2e68fa49
semantic-release <semantic-release>
2019-03-06 20:31:32 +0100

    4.1.6
    Automatically generated by python-semantic-release

--- semantic_release/__init__.py
+++ semantic_release/__init__.py
@@ -1,6 +1,6 @@
 """Semantic Release
 """
-__version__ = '4.1.5'
+__version__ = '4.1.6'
 
 from .errors import (ImproperConfigurationError, SemanticReleaseBaseError,
                      UnknownCommitMessageStyleError)
